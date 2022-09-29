a235195c0e19e21462d91aef31052b2a87e4e673
Laurier StAubin <st-aubinlaurier@hotmail.com>
2022-09-29 13:27:43 -0400

    setup state for active nav menu when clicking icon menu

---
+++ src/Components/Menu.js
@@ -0,0 +1,13 @@
+export default function Menu() {
+  return (
+    <div className="Menu">
+    <ul className="MenuList">
+      <li>About</li>
+      <li>Art</li>
+      <li>Music</li>
+      <li>Design</li>
+      <li>Contact</li>
+    </ul>
+  </div>
+  );
+}
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,17 +1,26 @@
+import React, { useState } from 'react';
+import Menu from './Menu.js';
+
 const menuIcon = "images/IconMenu.svg";
 
 function Navbar() {
+  const [menuActive, setMenuActive] = useState(false);
+
   const handleClick = (event) => {
     event.preventDefault();
-    console.log("clicked");
+    setMenuActive(!menuActive);
   }
 
   return (
-    <div className="Navbar">
-      <div className="NavbarIcons" onClick={handleClick}>
-        <p className="MenuIcon">Menu</p>
-        <img src={menuIcon} alt="Menu Icon" className="MenuIcon"/>
+    <div className="Navigation">
+      <div className="Navbar">
+        <div className="NavbarIcons" onClick={handleClick}>
+          <p className="MenuIcon">Menu</p>
+          <img src={menuIcon} alt="Menu Icon" className="MenuIcon"/>
+        </div>
       </div>
+      {menuActive ? <Menu/> : null }
+
     </div>
   );
 }
