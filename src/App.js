a4a9d090a10acbe9c5ada4d16461d02ea83914e3
Laurier StAubin <st-aubinlaurier@hotmail.com>
2022-09-29 15:16:30 -0400

    Hero section completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-// import logo from './logo.svg';
 import './Assets/CSS/main.css';
 import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+
+const video = '/videos/LordGigabrain.mp4'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Hero/>
     </div>
   );
 }
---
+++ src/Components/Hero.js
@@ -0,0 +1,21 @@
+const clone = "images/HeroImageFull.png";
+
+export default function Hero() {
+  return (
+    <div className="Hero">
+      <div className="HeroInfo">
+        <h3 className="InfoElement">A showcase of my creative work</h3>
+        <h2 className="InfoElement">LAURIER <strong>ST-AUBIN</strong></h2>
+        <p className="InfoElement">I love art, music, and technology. 
+          I am currently living in Canada, 
+          but I am looking to relocate to the metaverse. 
+          I can help you design and implement your website. 
+          I can make art and music for you. 
+          I can help bring your creative vision to the next level. 
+          Reach out to me!
+        </p>
+      </div>
+      <img src={clone} alt="Clone" className="CloneImage"></img>
+    </div>
+  );
+}
