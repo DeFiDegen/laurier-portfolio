f25ef32c65cf2e029f06a157c2eb7935400d288e
Laurier StAubin <st-aubinlaurier@hotmail.com>
2022-10-06 11:32:34 -0400

    layout all set for MaisonCarousel.js

--- src/Components/MaisonCarousel.js
+++ src/Components/MaisonCarousel.js
@@ -1,6 +1,13 @@
-export default function MaisonLaurierHero() {
+export default function MaisonCarousel() {
   return (
     <div id="MaisonCarousel">
+      <div className="CarouselBanner">
+        ART
+      </div>
+      <div className="Carousel">
+        <div className="CarouselItem"></div>
+      </div>
+
     </div>
   );
 }
