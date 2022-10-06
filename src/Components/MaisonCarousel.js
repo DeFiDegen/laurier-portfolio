860426113a377bb1fb9e1c5e4c37f996133f7a66
Laurier StAubin <st-aubinlaurier@hotmail.com>
2022-10-06 13:00:21 -0400

    Added all carousel items with layout styling

--- src/Components/MaisonCarousel.js
+++ src/Components/MaisonCarousel.js
@@ -1,3 +1,10 @@
+const riftCube = '/videos/RiftCube.mp4'
+const friends = '/images/Friends.JPEG'
+const bandit = '/images/Bandit.JPEG'
+const doNotPanic = '/images/DoNotPanic.JPEG'
+const alienGarden = '/images/AlienGarden.PNG'
+const clouds = '/images/Clouds.PNG'
+
 export default function MaisonCarousel() {
   return (
     <div id="MaisonCarousel">
@@ -5,9 +12,31 @@ export default function MaisonCarousel() {
         ART
       </div>
       <div className="Carousel">
-        <div className="CarouselItem"></div>
+        {/* <div className="CarouselItem Left">
+          <h6 className="CarouselText Left">RIFTCUBE</h6>
+          <video className="CarouselImage Left" src={riftCube} autoPlay loop muted></video>
+        </div> */}
+        {/* <div className="CarouselItem Right">
+          <img className="CarouselImage Right" src={friends} alt="Friends"></img>
+          <h6 className="CarouselText Right">FRIENDS</h6>
+        </div> */}
+        {/* <div className="CarouselItem Left">
+          <h6 className="CarouselText Left">BANDIT</h6>
+          <img className="CarouselImage Left" src={bandit} alt="Bandit"></img>
+        </div> */}
+        {/* <div className="CarouselItem Right">
+          <img className="CarouselImage Right" src={doNotPanic} alt="Do Not Panic"></img>
+          <h6 className="CarouselText Right">DO NOT PANIC</h6>
+        </div> */}
+        {/* <div className="CarouselItem Left">
+          <h6 className="CarouselText Left">ALIEN GARDEN</h6>
+          <img className="CarouselImage Left" src={alienGarden} alt="Alien Garden"></img>
+        </div> */}
+        {/* <div className="CarouselItem Right">
+          <img className="CarouselImage Right" src={clouds} alt="Clouds"></img>
+          <h6 className="CarouselText Right">CLOUDS</h6>
+        </div> */}
       </div>
-
     </div>
   );
 }
