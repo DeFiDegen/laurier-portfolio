98922655ef41e65ab7a18276c02f87acd10ca632
Laurier StAubin <st-aubinlaurier@hotmail.com>
2022-10-06 09:33:06 -0400

    created necessary files... ei. MaisonCarousel.js. also imported files to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './Assets/CSS/main.css';
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import MaisonLaurierHero from "./Components/MaisonLaurierHero";
+import MaisonCarousel from "./Components/MaisonCarousel";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <MaisonLaurierHero/> 
+      <MaisonCarousel/> 
     </div>
   );
 }
---
+++ src/Components/MaisonCarousel.js
@@ -0,0 +1,6 @@
+export default function MaisonLaurierHero() {
+  return (
+    <div id="MaisonCarousel">
+    </div>
+  );
+}
