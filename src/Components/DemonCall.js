943c11ba7128ba320c04ebf907c294ddbd06b527
Laurier StAubin <st-aubinlaurier@hotmail.com>
2022-10-26 14:26:29 -0400

    completed demon siteCards

--- src/Components/DemonCall.js
+++ src/Components/DemonCall.js
@@ -2,16 +2,18 @@ import React, { useState } from 'react';
 
 const NFTradeLogo = "/images/NFTradeLogo.svg"
 const driplLogo = "/images/DriplLogo.svg"
+const tartareFinanceLogo = "/images/TartareFinanceLogo.svg"
 
 export default function DemonCall() {
   const [NFTradeActive, setNFTradeActive] = useState(false);
   const [DriplActive, setDriplActive] = useState(false);
+  const [TartareFinanceActive, setTartareFinanceActive] = useState(false);
 
   return (
     <div id="DemonCall">
-      {/* <div className="DemonBanner">
+      <div className="DemonBanner">
         DESIGN
-      </div> */}
+      </div>
       <div className="SiteCards">
         <div className="SiteCard" 
         onMouseEnter={() => setNFTradeActive(true)} 
@@ -28,6 +30,18 @@ export default function DemonCall() {
           <div className="SCText">Dripl</div>
           <img src={driplLogo} alt="Dripl Logo" className={DriplActive ? "ActiveImage" : "InactiveImage"} />
         </div>
+
+        <div className="SiteCard" 
+        onMouseEnter={() => setTartareFinanceActive(true)} 
+        onMouseLeave={() => setTartareFinanceActive(false)}
+        >
+          <div className="SCText">TartareFinance</div>
+          <img src={tartareFinanceLogo} alt="TartareFinance Logo" className={TartareFinanceActive ? "ActiveImage" : "InactiveImage"} />
+        </div>
+      </div>
+      <div>
+        <p><em>Please reach out if you want to collaborate</em></p>
+        <div className='ReachButton'></div>
       </div>
     </div>
   );
