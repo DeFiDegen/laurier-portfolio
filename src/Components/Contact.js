b6e7a3d76fdb3e08ab9432576030ca17b114ee33
Laurier StAubin <st-aubinlaurier@hotmail.com>
2022-11-07 15:07:16 -0500

    all form logic completed. Added prompt to let the user know if the message was succesfully sent. Send user info through EmailJS

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,18 +1,61 @@
+import emailjs from '@emailjs/browser';
+import React, { useState } from 'react';
+
+import Success from './Success';
+import Failure from './Failure';
+
 const twitter = "images/Twitter.svg";
 const instagram = "images/Instagram.svg";
 const behance = "images/Behance.svg";
 const gitHub = "images/GitHub.svg";
 
 export default function Contact() {
+  const [success, setSuccess] = useState(false);
+  const [failure, setFailure] = useState(false);
+
+  const resetForm = (event) => {
+    event.target["user_name"].value = "";
+    event.target["user_email"].value = "";
+    event.target["user_subject"].value = "";
+    event.target["message"].value = "";
+  };
+
+  const delay = ms => new Promise(res => setTimeout(res, ms));
+
+  const successCountdown = async() => {
+    setSuccess(true);
+    await delay(3000);
+    setSuccess(false);
+  };
+
+  const failureCountdown = async() => {
+    setFailure(true);
+    await delay(3000);
+    setFailure(false);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const contactPayload = {};
-    contactPayload["name"] = event.target["user_name"].value;
-    contactPayload["email"] = event.target["user_email"].value;
-    contactPayload["subject"] = event.target["user_subject"].value;
-    contactPayload["message"] = event.target["message"].value;
-    console.log(contactPayload);
+
+    const contactPayload = {
+      name: event.target["user_name"].value,
+      email: event.target["user_email"].value,
+      subject: event.target["user_subject"].value,
+      message: event.target["message"].value
+    };
+
+    if (contactPayload["name"] && contactPayload["email"] && contactPayload["subject"] && contactPayload["message"]) {
+      emailjs.send("service_f08ooe8","template_edp48fo", contactPayload, "user_c1XSQZnKnV3t0QkiJmWPA")
+        .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+        successCountdown();
+        resetForm(event);
+      }, function(error) {
+        console.log('FAILED...', error);
+      });
+    } else {
+      failureCountdown();
+    }
   }
 
   return (
@@ -41,6 +84,8 @@ export default function Contact() {
         </div>
         <div className="RightContact">
           <div className="ContactText">CONTACT</div>
+          {success ? <Success /> : null }
+          {failure ? <Failure /> : null }
           <form className="ContactForm" onSubmit={handleSubmit}>
             <label className="FormLabel">Name</label>
             <input className="FormInput" type="text" name="user_name"/>
